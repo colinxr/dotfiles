76809ebb492e185a16a3e2b3c3014b326001718e
Colin Rabyniuk <colin@madebyarticle.com>
2019-11-22 12:30:19 -0500

    edit install script; add readme

--- new_install.sh
+++ new_install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo Let\'s install some apps!
+echo -e "\u001b[32;1m Let's install some apps!\u001b[0m"
 
-echo Install Homebrew
+echo -e "\u001b[32;1m Install Homebrew\u001b[0m"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # nvm
-echo Install NVM
+echo -e "\u001b[32;1m Install NVM\u001b[0m"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-echo Nnstall Node
+echo -e "\u001b[32;1m Install Node\u001b[0m"
 nvm install node
 
 brew tap caskroom/cask
@@ -16,7 +16,7 @@ export HOMEBREW_CASK_OPTS="--appdir-/Applications"
 xcode-select --install
 
 # install Applications
-echo Install our applications
+echo -e "\u001b[32;1m Install our applications\u001b[0m"
 ## development
 brew cask install iterm2
 brew cask install visual-studio-code
@@ -35,28 +35,29 @@ brew cask install dropbox
 brew cask install vanilla
 brew cask install google-drive
 
-
 brew cask install google-chrome
 brew cask install firefox
 
 brew cask install sketch
 brew cask install spotify
 
-echo Set up iTerm & zsh
+echo -e "\u001b[32;1m Set up iTerm & zsh \u001b[0m"
 brew install zsh
 brew install zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+cp .zshrc ~/.zshrc
 
-echo Install WP-Cli
+
+echo -e "\u001b[32;1m Install WP-Cli \u001b[0m"
 # wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php wp-cli.phar --info
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
-echo install VSCode extensions
+echo -e "\u001b[32;1m Install VSCode extensions \u001b[0m"
 code --install-extension gitlens
 code --install-extension vscode-pigments
 code --install-extension sass
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Dotfiles
+
+Run ./new_install.sh to set up a new machine. 
+
+Included configurations:
+- zsh
+- powerline10k
+- vscode settings
+
+Fonts:
+- Fire Code
+- Hack
+- Menlo
+- Monaco
+
+
