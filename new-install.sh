198d3088821b9ab102403e8f3c8e0b1318423b3a
Colin Rabyniuk <colinxr@gmail.com>
2025-03-22 13:37:27 -0700

    new-install updates

--- new-install.sh
+++ new-install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-echo -e "\u001b[32;1mLet's install some apps!\u001b[0m"
+echo -e "Let's install some apps!"
 
 # Store the absolute path to the config directory
 CONFIG_DIR=$(pwd)
-echo -e "\u001b[32;1mConfig directory: $CONFIG_DIR\u001b[0m"
+echo -e "Config directory: $CONFIG_DIR"
 
-echo -e "\u001b[32;1mInstall Homebrew\u001b[0m"
+echo -e "Install Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # nvm
-echo -e "\u001b[32;1mInstall NVM\u001b[0m"
+echo -e "Install NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.zsh | zsh
-echo -e "\u001b[32;1mInstall Node\u001b[0m"
+echo -e "Install Node"
 nvm install node
 
 brew tap homebrew/cask
@@ -21,7 +21,7 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 xcode-select --install
 
 # install Applications
-echo -e "\u001b[32;1mInstall our applications\u001b[0m"
+echo -e "Install our applications"
 ## development
 brew install ghostty
 brew install --cask cursor
@@ -53,7 +53,6 @@ brew install --cask arc
 brew install --cask spotify
 brew install --cask git-credential-manager
 
-echo -e "\u001b[32;1mSet up iTerm & zsh \u001b[0m"
 brew install zsh
 brew install zsh-syntax-highlighting
 
@@ -63,7 +62,7 @@ brew install lazygit
 brew install jesseduffield/lazydocker/lazydocker
 brew install lazydocker
 
-echo -e "\u001b[32;1mCreate symlinks for configuration files\u001b[0m"
+echo -e "Create symlinks for configuration files"
 # Backup existing config files
 mkdir -p ~/.config_backup
 
@@ -81,43 +80,20 @@ fi
 ln -sf "$CONFIG_DIR/git/gitconfig" ~/.gitconfig
 echo "Symlinked gitconfig"
 
-# Neovim config
-if [ -d ~/.config/nvim ]; then
-  mv ~/.config/nvim ~/.config_backup/nvim.backup
-fi
-mkdir -p ~/.config
-ln -sf "$CONFIG_DIR/nvim" ~/.config/nvim
-echo "Symlinked nvim config"
-
 # Ghostty config
-mkdir -p ~/.config/ghostty
-if [ -f ~/.config/ghostty/config ]; then
-  mv ~/.config/ghostty/config ~/.config_backup/ghostty.config.backup
-fi
-ln -sf "$CONFIG_DIR/ghostty" ~/.config/
+mkdir -p ~/Library/Application\ Support/Ghostty
+
+ln -sf "$CONFIG_DIR/ghostty/config" ~/Library/Application\ Support/Ghostty/config
 echo "Symlinked ghostty config"
 
 # Tmux config
 if [ -f ~/.tmux.conf ]; then
   mv ~/.tmux.conf ~/.config_backup/.tmux.conf.backup
 fi
-if [ -d ~/.tmux/plugins ]; then
-  mv ~/.tmux/plugins ~/.config_backup/tmux_plugins.backup
-fi
-mkdir -p ~/.tmux
-ln -sf "$CONFIG_DIR/tmux/plugins" ~/.tmux/plugins
-# Create a basic tmux.conf that sources your plugins if needed
-# echo "source $CONFIG_DIR/tmux/plugins/catppuccin/catppuccin.tmux" > ~/.tmux.conf
-echo "Symlinked tmux plugins"
+ln -sf "$CONFIG_DIR/tmux/tmux.conf" ~/.tmux.conf
+echo "Symlinked tmux.conf"
 
-# Raycast config
-if [ -f ~/.config/raycast.rayconfig ]; then
-  mv ~/.config/raycast.rayconfig ~/.config_backup/raycast.rayconfig.backup
-fi
-ln -sf "$CONFIG_DIR/raycast.rayconfig" ~/.config/raycast.rayconfig
-echo "Symlinked raycast config"
-
-echo -e "\u001b[32;1mInstall VSCode/Cursor extensions \u001b[0m"
+echo -e "Install VSCode/Cursor extensions "
 cursor --install-extension gitlens
 cursor --install-extension vscode-pigments
 cursor --install-extension sass-indented
@@ -141,4 +117,4 @@ code --install-extension mgmcdermott.vscode-language-babel
 # brew clean up
 brew cleanup
 
-echo -e "\u001b[32;1mSetup complete! You may want to restart your terminal.\u001b[0m"
+echo -e "Setup complete! You may want to restart your terminal."
